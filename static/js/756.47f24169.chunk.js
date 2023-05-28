f2b67566c50fc3e687bd369f85ee5f8c6eb6a835
AntoninaKobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-05-28 07:37:31 +0000

    Deploying to gh-pages from @ AntoninaKobzar/goit-react-hw-08-phonebook@edac94c69101cb63e1ba3c2522c26cae5978fee6 🚀

---
+++ static/js/756.47f24169.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[756],{756:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("h1",{children:"Register"})})}}}]);
+//# sourceMappingURL=756.47f24169.chunk.js.map
